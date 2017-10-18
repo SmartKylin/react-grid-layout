95683b8dd8f6d6eef209975cd3437858f2eee466
Torkel Ödegaard <torkel@grafana.com>
2017-10-18 21:34:18 +0200

    got compaction & move element out of the way to work as expected

--- test/examples/0-showcase.jsx
+++ test/examples/0-showcase.jsx
@@ -139,6 +139,20 @@ function generateLayout(change) {
   ];
 }
 
+// function generateLayout() {
+//   return _.map(_.range(0, 25), function (item, i) {
+//     var y = Math.ceil(Math.random() * 4) + 1;
+//     return {
+//       x: _.random(0, 5) * 2 % 12,
+//       y: Math.floor(i / 6) * y,
+//       w: 2,
+//       h: y,
+//       i: i.toString(),
+//       static: Math.random() < 0.05
+//     };
+//   });
+// }
+
 if (require.main === module) {
   require('../test-hook.jsx')(module.exports);
 }
